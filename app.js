204dcb9a075a9839a3a3f119b18e4ee583641c42
sergey <serge.dmitriev@gmail.com>
2014-11-30 23:55:13 +0700

    check sip.conf file example

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ asterisk.ami.on('error', function (err) {
 
 function general(){
   var p = asterisk.ini['sip.conf'].general;
-  console.log(asterisk.ini['sip.conf'].general);
+  console.log(p);
+  if(p){
+    if(p.transport == 'udp'){
+      console.log('transport udp true');
+    }
+    if(Array.isArray(p.register)){
+      console.log('sip.conf [general] contain register, count:', p.register.length);
+    }
+  }
   console.log(ini.encode(p));
-}
+};
